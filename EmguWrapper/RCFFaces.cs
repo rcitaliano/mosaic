f7f646ba2fafd9649b8c749fb35e702697a356cd
robson <rcitaliano@gmail.com>
2018-10-27 13:50:48 +0200

    added emgu cuda support

---
+++ EmguWrapper/RCFFaces.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmguWrapper
+{
+    public class RCFFaces
+    {
+        public static void GetFaceRectangle(String filePath)
+        {
+
+        }
+    }
+}
