e00c39a17612af11df4f6447efa61a61ae2c4102
robson <rcitaliano@gmail.com>
2018-11-18 23:44:53 +0100

    sample code to run the program

--- Mosaic/Program.cs
+++ Mosaic/Program.cs
@@ -18,8 +18,9 @@ namespace Mosaic
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            DirectoryInfo dir = new DirectoryInfo("C:\\Users\\Public\\Pictures\\HeM FOTOS");
+            //Application.Run(new CutImages("C:\\Users\\Public\\Pictures\\hem nuove"));
+            //return;
+            DirectoryInfo dir = new DirectoryInfo("C:\\Users\\Public\\Pictures\\HEM tagliate");
             //ImageHelper.ImageHelper.TestImage(
             //    (Bitmap)Image.FromFile("base.png"),
             //    0.5f).Save("test.png", System.Drawing.Imaging.ImageFormat.Png);
